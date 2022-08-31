789f9d5dee898732cca01d7f6a43dcf37d84f765
hostedposted <hostedpostedsite@gmail.com>
2022-08-31 11:26:25 -0400

    Google Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,24 @@
 import { Html, Main, NextScript, Head } from "next/document"
+import Script from "next/script"
 
 const MainDocument = (): JSX.Element => {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-77D52LPYJ9"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){window.dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'G-77D52LPYJ9');
+                    `}
+                </Script>
+            </Head>
             <body>
                 <Main />
                 <NextScript />
